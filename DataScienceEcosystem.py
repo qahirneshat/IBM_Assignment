64dad9ba56c6c2703f1ba22f032c24e1b39ea020
qahirneshat <140306065+qahirneshat@users.noreply.github.com>
2023-07-23 15:10:12 +0500

    Adding the python version of the Assignment
    Ipynb version wasn't getting displayed, so I am uploading the python version here.

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized. I am also trying to add some more texts here so that it looks original, not just copy-paste. You fellow students can understand what I am talking about.
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - Famous used libraries
+# - Ordered List
+# - Unordered list
+# - Arithmathic Operations
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. Scala
+# 4. SQL
+# 5. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Scikit-learn
+# 2. Pandas
+# 3. Pandas
+# 4. Scipy
+# 5. Matplotlib
+# 6. Seaborn
+# 7. GGPLOT
+
+# | Data Science Tools |
+# |:---------- |
+# | 1. Jupyter Notebook |
+# | 2. RStudio |
+# | 3. JupyterLab |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# - For Addition use this symbol +
+# - For Subtraction use this symbol -
+# - For Division use this symbol /
+# - For Multiplicaiton use this symbol *
+
+# In[2]:
+
+
+#  This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+minutes = 200
+seconds = 60
+hour = minutes / seconds
+
+print(hour)
+
+
+# ## Author :
+# Qahir Neshat
+
+# In[ ]:
+
+
+
+
